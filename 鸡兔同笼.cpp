9f2820a80dd73e31f04501f06db6f979cd78a181
Jrojro728 <64339010+Jrojro728@users.noreply.github.com>
2022-02-25 15:13:17 +0800

    Add files via upload

--- 鸡兔同笼.cpp
+++ 鸡兔同笼.cpp
@@ -1,6 +1,9 @@
-﻿#include <iostream>
-
-int call;
+﻿// 鸡兔同笼v1.0.1.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+#include <iostream>
+#include <string>
+#include <cstdio>
+#include <cstdlib>
+#include <fstream>
 
 int afor(int x, int y)
 {
@@ -9,33 +12,48 @@ int afor(int x, int y)
 
 int main()
 {
+    int call;
     int temp;
     int a;
     int x{};
     int y{};
+    int Normal_exit{};
+
 
     std::cout << "输入脚的总数：";
     std::cin >> temp;
-    const int jiao{temp};
+    const int jiao{ temp };
 
     std::cout << "输入有多少头：";
     std::cin >> a;
 
     x = a;
-    for (call = 0;call < 10000;call++)
+    for (call = 0; call < 10000; call++)
     {
-        temp = afor(x,y);
-        if (jiao == temp)
+        temp = afor(x, y);
+        if (x < 0)
+        {
+            Normal_exit = 0;
+            break;
+        }
+        else if(temp == jiao)
         {
             std::cout << "========结果========\n";
             std::cout << "鸡的数量是：" << x << "\n" << "兔子的数量是：" << y << "\n" << "运算了" << call << "次" << "\n";
+            Normal_exit = 1;
             break;
         }
         else
         {
-            x = x - 1;
-            y = y + 1;
+            x -= 1;
+            y += 1;
         }
+    }
+    if (Normal_exit != 1)
+    {
+        std::cout << "==================错误===================\n";
+        std::cout << "程序未正常求出结果\n";
+        std::cout << "解决方法:重新打开程序并检查是否输入错误\n";
     }
     system("pause");
 
